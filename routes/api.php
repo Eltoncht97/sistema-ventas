b650c6d8773822d358d554ba57ad0c47aea0fe80
Elton Chavez <elton.mct@gmail.com>
2021-05-22 11:45:38 -0300

    no auth

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('login', [\App\Http\Controllers\UserController::class, 'Login'])->name('login');
 
-Route::middleware(['auth:api'])->group(function () {
+// Route::middleware(['auth:api'])->group(function () {
     Route::apiResource('productos', \App\Http\Controllers\ProductoController::class);
     Route::put('set-like/{producto}', [\App\Http\Controllers\ProductoController::class, 'setLike'])->name('set-like');
     Route::put('set-dislike/{producto}', [\App\Http\Controllers\ProductoController::class, 'setDislike'])->name('set-dislike');
     Route::put('set-imagen/{producto}', [\App\Http\Controllers\ProductoController::class, 'setImagen'])->name('set-imagen');
     Route::post('logout', [\App\Http\Controllers\UserController::class, 'Logout'])->name('logout');
-});
+// });
 
